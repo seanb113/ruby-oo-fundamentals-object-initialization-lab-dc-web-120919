aaa1a621d19defb101c94f3a11d579847424d2ef
Sean Beach <sean.beach1@yahoo.com>
2019-12-11 03:03:26 +0000

    Automatically backed up by Learn

---
+++ lib/dog.rb
@@ -0,0 +1,15 @@
+class Dog
+  def initialize(breed)
+    @breed = breed
+  end
+  def breed=(breed)
+    @breed = breed
+  end
+  def breed
+    @breed
+  end
+  if @breed = |nil|
+    breed = "Mutt"
+  end
+end
+  
